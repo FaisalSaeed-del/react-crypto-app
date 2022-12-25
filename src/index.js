635580230ae5263d9ded35ac6f8051a6c639b0fa
mohsin_ox <mohsin@oxhain.com>
2022-12-25 11:26:25 +0500

    completed

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import { ColorModeScript } from '@chakra-ui/react';
-import React, { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ChakraProvider, theme } from '@chakra-ui/react';
 
-const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container);
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <StrictMode>
-    <ColorModeScript />
-    <App />
-  </StrictMode>
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
 );
+
+export const server = `https://api.coingecko.com/api/v3`;
